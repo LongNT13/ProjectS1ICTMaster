22493f5920cf4769e928c9ab14f8a224729e8bb3
LongNT13 <tanguyenlongntt@gmail.com>
2019-06-16 23:46:41 +0700

    Fix Task does not add to List when reaching release time

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -24,6 +24,7 @@ public class Scheduler {
     private List<Task> taskList;
     private List<String> inCompleteList;
     private List<Integer> inCompleteTime;
+    private List<Integer> inCompleteDeadline;
     private int currentTime;
 
     public Scheduler(String fileName) {
@@ -31,6 +32,7 @@ public class Scheduler {
         currentTime = 0;
         inCompleteList = new ArrayList<>();
         inCompleteTime = new ArrayList<>();
+        inCompleteDeadline = new ArrayList<>();
         //read task from file
         File file = new File(fileName);
         try {
@@ -39,7 +41,9 @@ public class Scheduler {
             String st;
             while ((st = bf.readLine()) != null) {
                 String tempList[] = st.split("\t");
-                Task tempTask = new Task(tempList[0], Integer.parseInt(tempList[1]), Integer.parseInt(tempList[2]), Integer.parseInt(tempList[3]), Integer.parseInt(tempList[4]));
+                Task tempTask = new Task(tempList[0], Integer.parseInt(tempList[1]), 
+                    Integer.parseInt(tempList[2]), Integer.parseInt(tempList[3]), 
+                    Integer.parseInt(tempList[4]));
                 taskList.add(tempTask);
             }
         } catch (FileNotFoundException ex) {
@@ -50,39 +54,8 @@ public class Scheduler {
 
     }
 
-    public List<Task> getTaskList() {
-        return taskList;
-    }
-
-    public void setTaskList(List<Task> taskList) {
-        this.taskList = taskList;
-    }
-
-    public List<String> getInCompleteList() {
-        return inCompleteList;
-    }
-
-    public void setInCompleteList(List<String> inCompleteList) {
-        this.inCompleteList = inCompleteList;
-    }
-
-    public int getCurrentTime() {
-        return currentTime;
-    }
-
-    public void setCurrentTime(int currentTime) {
-        this.currentTime = currentTime;
-    }
-
-    public List<Integer> getInCompleteTime() {
-        return inCompleteTime;
-    }
-
-    public void setInCompleteTime(List<Integer> inCompleteTime) {
-        this.inCompleteTime = inCompleteTime;
-    }
-
     public void startSimulation(int time) {
+
         //re-initialize variables
         currentTime = 0;
         boolean isFirst = true;
@@ -90,22 +63,24 @@ public class Scheduler {
         //start simulation
         while (time >= currentTime) {
             System.out.println(currentTime);
-            //check current time for new task
+            //check current time for new tasks
             if (isFirst) {
+                isFirst = false;
                 for (Task task : taskList) {
                     if (task.getReleaseTime() == 0) {
                         inCompleteList.add(task.getTaskName());
                         inCompleteTime.add(task.getTaskWcet());
+                        inCompleteDeadline.add(task.getDeadline());
                     }
                 }
+                currentTime++;
+                continue;
             } else {
-                isFirst = false;
                 for (Task task : taskList) {
                     //check if task has been released (time)
                     if (currentTime >= task.getReleaseTime()) {
-
                         //check period
-                        if (task.getTaskPeriod() % (currentTime - task.getReleaseTime()) == 0) {
+                        if (task.getTaskPeriod() % (currentTime - task.getReleaseTime()) >= task.getTaskPeriod()) {
                             boolean isInIncompleteList = false;
                             //check if task is in incomplete List
                             for (String string : inCompleteList) {
@@ -116,60 +91,84 @@ public class Scheduler {
                             }
                             //add to incomplete List if doesnt exist
                             if (!isInIncompleteList) {
-
+                                System.out.println("add "+task.getTaskName());
                                 inCompleteList.add(task.getTaskName());
                                 inCompleteTime.add(task.getTaskWcet());
+                                inCompleteDeadline.add(task.getDeadline());
                             }
                         }
                     }
                 }
             }
-
             //end check new task
-            //check deadline of all task in incomplete List
-            int smallestDeadline = inCompleteTime.get(0);
-            int count = 1;
-            for (Integer deadlineTime : inCompleteTime) {
-                if (deadlineTime < smallestDeadline) {
-                    count = 1;
-                    smallestDeadline = deadlineTime;
-                } else if (deadlineTime == smallestDeadline) {
-                    count++;
-                }
-            }
-            //end check
 
-            Random rand = new Random();
-            int n = rand.nextInt(count);
+            if (inCompleteList.size()>0) {
 
-            count = 0;
-            // decrease time base on earliest deadline
-            for (int i = 0; i < inCompleteTime.size(); i++) {
-                if (inCompleteTime.get(i) == smallestDeadline) {
-                    if (count == n) {
-                        inCompleteTime.set(i, inCompleteTime.get(i) - 1);
-                        if (inCompleteTime.get(i) == 0) {
-                            System.out.println(inCompleteList.get(i) + " has done the job at " + currentTime);
-                            inCompleteList.remove(i);
-                            inCompleteTime.remove(i);
-                            break;
+                //check deadline of all task in incomplete List
+                int count = 0;
+                int smallestDeadline = -1;
+                if (inCompleteList.size()>0) {
+                    smallestDeadline = inCompleteDeadline.get(0);
+                    for (Integer deadlineTime : inCompleteDeadline) {
+                        if (deadlineTime < smallestDeadline) {
+                            count = 1;
+                            smallestDeadline = deadlineTime;
+                        } else if (deadlineTime == smallestDeadline) {
+                            count++;
                         }
-                    } else {
-                        count++;
+                    }                            
+                }
+                //end check
+
+                
+                // decrease time base on earliest deadline
+                System.out.println("smallestDeadline : " + smallestDeadline);
+                Random rand = new Random();
+                int n = rand.nextInt(count);
+                count = 0;
+
+                for (int i = 0; i < inCompleteList.size(); i++) {
+                    if (inCompleteDeadline.get(i) == smallestDeadline) {
+                        if (count == n) {
+                            System.out.println(inCompleteList.get(i) + " time needed to finish: " + inCompleteTime.get(i) +" - 1");
+                            inCompleteTime.set(i, inCompleteTime.get(i) - 1);
+                            if (inCompleteTime.get(i) == 0) {
+                                System.out.println(inCompleteList.get(i) + " has done the job at " + currentTime);
+                                inCompleteList.remove(i);
+                                inCompleteTime.remove(i);
+                                inCompleteDeadline.remove(i);
+                                break;
+                            }
+                        } else {
+                            count++;
+                        }
                     }
                 }
+                //---------
+
+                //update deadline
+                for (int i = 0; i < inCompleteList.size() ; i++) {
+                    inCompleteDeadline.set(i, inCompleteDeadline.get(i) - 1);
+                    if (inCompleteDeadline.get(i) <= 0) {
+                        System.out.println("remove deadline " + inCompleteList.get(i));
+                        inCompleteList.remove(i);
+                        inCompleteTime.remove(i);
+                        inCompleteDeadline.remove(i);
+                    }
+                }
+                //end update deadline    
             }
-            //---------
+            
 
             //increase current time
             currentTime++;
+            System.out.println("");
         }
     }
 
     public static void main(String[] args) {
         if (args.length == 2) {
             Scheduler sc = new Scheduler(args[0]);
-            System.out.println(sc.getTaskList());
             sc.startSimulation(Integer.parseInt(args[1]));
         }
     }
